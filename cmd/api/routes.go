9f98f312dcc87abcf03c78e28a8db42cea1f6587
Dev Dhanadiya <devxdhanadiya@gmail.com>
2026-07-22 22:59:42 +0530

    added GetJobByID functionality

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,8 @@ import (
 
 func (app *application) routes(mux *http.ServeMux) {
 	api := apihandler.New(app.db)
+
 	mux.HandleFunc("GET /", api.HomeHandler)
 	mux.HandleFunc("POST /api/job", api.CreateJob)
+	mux.HandleFunc("GET /api/job/{id}", api.GetJobByID)
 }
